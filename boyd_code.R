a41922da00be7be1ee21c1fb33e0db18b0a036f6
Matthew <59383052+mattboyd27@users.noreply.github.com>
2022-02-28 21:43:33 -0700

    Knn code and submission csv

--- boyd_code.R
+++ boyd_code.R
@@ -1,19 +1,35 @@
 library(tidyverse)
 library(class)
 library(caret)
+library(ggpubr)
 df = read.csv("~/DSCI 478/Kaggle Project/digit-recognizer/train.csv")
 test = read.csv("~/DSCI 478/Kaggle Project/digit-recognizer/test.csv")
 
 # Visualize different numbers by row
-df %>%
-  slice(15) %>%
-  select(starts_with("pixel")) %>%
-  pivot_longer(starts_with("pixel")) %>%
-  mutate(x = (row_number() - 1) %% 28,
-         y = -((row_number() - 1) %/% 28)) %>%
-  ggplot(aes(x = x, y = y, fill = value)) +
-  geom_tile()
+image = function(row) {
+  df %>%
+    slice(row) %>%
+    select(starts_with("pixel")) %>%
+    pivot_longer(starts_with("pixel")) %>%
+    mutate(x = (row_number() - 1) %% 28,
+           y = -((row_number() - 1) %/% 28)) %>%
+    ggplot(aes(x = x, y = y, fill = value)) +
+    geom_tile() +
+    theme_void() +
+    scale_fill_gradient(high = "black",
+                        low = "gray80") +
+    guides(fill = "none")
+}
 
+one = image(15)
+
+two = image(16) 
+
+three = image(150)
+
+four = image(1600)
+
+ggarrange(one, two, three, four, ncol = 2, nrow = 2)
 # Training data count of different labels
 df %>%
   group_by(label) %>%
@@ -78,7 +94,26 @@ for(i in 1:5) {
 knn_df %>%
   group_by(k) %>%
   summarize(accuracy = mean(accuracy)) %>%
-  arrange(desc(accuracy))
+  arrange(desc(accuracy)) %>%
+  ggplot(aes(x = k, y = accuracy))+
+  geom_line() +
+  geom_point() +
+  ylim(0.95, 0.97) +
+  labs(x = "K", y = "Cross Validation Accuracy",
+       title = "KNN Cross Validation Accuracy") +
+  theme_minimal()
+
+# Final
+response = df %>% select(label) %>% as.matrix()
+
+test = test %>% as.matrix()
+
+train = df %>% select(-label) %>% as.matrix()
+
+final_knn = knn(train, test, response, k = 1)
+
+final = data.frame(ImageId = 1:28000, Label = final_knn) 
 
 
 
+write.csv(final, "submission.csv")
